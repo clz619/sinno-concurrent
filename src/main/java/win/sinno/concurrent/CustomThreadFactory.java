e2f197e847e6e4097b07c9d80f00a61ca01c26af
lizhong.chen <admin@chenlizhong.cn>
2017-03-08 11:53:55 +0800

    [ADD] add thread factory

--- src/main/java/win/sinno/concurrent/CustomThreadFactory.java
+++ src/main/java/win/sinno/concurrent/CustomThreadFactory.java
@@ -1,38 +1,46 @@
-package win.sinno.concurrent.ant;
+package win.sinno.concurrent;
 
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * 修改池名称
+ * custom thread factory
  *
  * @author : admin@chenlizhong.cn
  * @version : 1.0
- * @since : 2016/10/25 上午9:56
+ * @since : 2017/3/7 下午2:45
  */
-public class DataThreadFactory implements ThreadFactory {
+public class CustomThreadFactory implements ThreadFactory {
+
     private static final AtomicInteger poolNumber = new AtomicInteger(1);
-    private final AtomicInteger threadNumber = new AtomicInteger(1);
     private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
     private final String namePrefix;
 
-    public DataThreadFactory(String name) {
+
+    public CustomThreadFactory(String name) {
+
         SecurityManager s = System.getSecurityManager();
+
         group = (s != null) ? s.getThreadGroup() :
                 Thread.currentThread().getThreadGroup();
-        namePrefix = "data-pool-[" + name + "]-" +
+
+        namePrefix = name + "-pool-" +
                 poolNumber.getAndIncrement() +
                 "-thread-";
     }
 
     public Thread newThread(Runnable r) {
+
         Thread t = new Thread(group, r,
                 namePrefix + threadNumber.getAndIncrement(),
                 0);
+
         if (t.isDaemon())
             t.setDaemon(false);
         if (t.getPriority() != Thread.NORM_PRIORITY)
             t.setPriority(Thread.NORM_PRIORITY);
         return t;
     }
+
 }
